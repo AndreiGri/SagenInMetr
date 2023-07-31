9a9af6dc81eed73574b61163e7385250c63699a3
Gridin_Andrei <andreigridin918@gmail.com>
2023-07-31 21:03:33 +0500

    Добавьте файлы проекта.

---
+++ SagenInMetr/SagenInMetr.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+double SagenInMetr(int sag) {
+    const double metrInSagen = 2.16;
+    double metr = sag * metrInSagen;
+    return metr;
+}
+
+int main()
+{
+    //Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+    int sagen;
+    cout << "Введите кол-во саженей: ";
+    cin >> sagen;
+    cout << "В " << SagenInMetr(sagen) << " метрах " << sagen << " саженей." << endl;
+    //Задержка консольного окна:
+    system("pause > nul");
+    return 0;
+}
